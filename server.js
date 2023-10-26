53764bd7b3ca694b7e769028ecbb05568c45bcb4
EsraaSyam <e294014@gmail.com>
2023-10-26 16:22:50 +0300

    server is running

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+
+// route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// listen
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});

6a16c99d6485e5718480a8bf18eb84d9cb0ad3e6
EsraaSyam <e294014@gmail.com>
2023-10-26 18:52:19 +0300

    create Schema for register

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+        minlength : 3,
+        maxlength : 50
+    },
+    id :{
+        type : number ,
+        required : true,
+        length : 14
+
+    }
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+const validateUser = (user) => {
+    const schema = joi.object({
+        name : joi.string().min(3).max(50).required(),
+        id : joi.number().length(14).required()
+    });
+    return schema.validate(user);
+}
+
+module.exports.User = User;
+module.exports.validateUser = validateUser;
+

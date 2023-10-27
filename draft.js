b7ecd31979d920eaf14e272bd839d8987415a812
EsraaSyam <e294014@gmail.com>
2023-10-27 16:05:48 +0200

    delete draft

--- draft.js
+++
@@ -1,7 +0,0 @@
-if (req.body.id) {
-    const user = await User.findOne({ id: req.body.id });
-    // Continue processing with 'user'
-  } else {
-    // Handle the case where 'req.body.id' is not defined
-  }
-  
